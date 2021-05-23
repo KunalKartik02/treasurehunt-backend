64ff259a66805f61a7d9472f0e75c522afc6e9a6
tonalmathew <tonalmathew1999@gmail.com>
2021-05-23 15:56:19 +0530

    questions post request done

--- controllers/questionController.js
+++ controllers/questionController.js
@@ -10,3 +10,14 @@ exports.getAllQuestions = async function (req, res) {
     res.status(404).json({ status: "fail", message: err });
   }
 };
+
+exports.createQuestions = async function (req, res) {
+  try {
+    const newQuestion = await questions.create(req.body);
+    res
+      .status(200)
+      .json({ status: res.statusCode, message: "OK", data: newQuestion });
+  } catch {
+    res.status(404).json({ status: "fail", message: err });
+  }
+};
--- router/questionRouter.js
+++ router/questionRouter.js
@@ -2,6 +2,9 @@ const express = require("express");
 const questionController = require("../controllers/questionController");
 const router = express.Router();
 
-router.route("/").get(questionController.getAllQuestions);
+router
+  .route("/")
+  .get(questionController.getAllQuestions)
+  .post(questionController.createQuestions);
 
 module.exports = router;
