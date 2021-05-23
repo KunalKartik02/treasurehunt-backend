64cb2218ae967be0e8ca8f8c38fd25101ffa86e5
tonalmathew <tonalmathew1999@gmail.com>
2021-05-23 12:48:55 +0530

    root route

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 const express = require("express");
-const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
 const cors = require("cors");
 const InitiateMongoServer = require("./db");
 
 InitiateMongoServer();
 
+const rootRouter = require("./router/rootRouter");
+const healthRouter = require("./router/healthRouter");
+
 const app = express();
 app.use(cors());
-app.use(bodyParser.json());
+app.use(express.json());
+
+app.use("/*", rootRouter);
+app.use("/health", healthRouter);
 
 dotenv.config({ path: "./config.env" });
 
