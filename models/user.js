f9f3ddb2c24f03ab5e5b423865d6cb7708269077
tonalmathew <tonalmathew1999@gmail.com>
2021-05-28 22:09:28 +0530

    updated user schema

--- models/user.js
+++ models/user.js
@@ -1,11 +1,17 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-  firstName: String,
-  lastName: String,
+  uId: String,
+  fullName: String,
   email: String,
   uCode: String,
+  answers: [
+    {
+      level: Number,
+      time: Date,
+    },
+  ],
 });
 
-const users = mongoose.model('users', userSchema);
+const users = mongoose.model("users", userSchema);
 module.exports = users;
