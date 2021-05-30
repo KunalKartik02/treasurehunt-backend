378a81e4ae2c238f8a89ae0b3dcf33c3f0665a10
tonalmathew <tonalmathew1999@gmail.com>
2021-05-30 14:28:29 +0530

    getUser by uid

--- controllers/userController.js
+++ controllers/userController.js
@@ -14,7 +14,7 @@ exports.getAllUsers = async function (req, res) {
 
 exports.getUser = async function (req, res) {
   await users
-    .find({ uCode: req.params.uid })
+    .find({ uId: req.params.uid })
     .select({ "uCode": 1 })
     .exec((error, result) => {
       if (error)
