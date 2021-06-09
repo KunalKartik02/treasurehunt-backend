4eca1b0673d6c2e19ed33da01241cceca859aa5a
shan-shaji <shan.shaji@yahoo.com>
2021-06-09 22:27:08 +0530

    feat: added answer list

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,7 +3,7 @@ const users = require("../models/user");
 
 exports.getAllUsers = async function (req, res) {
   try {
-    const allUsers = await users.find().select("-answers  -__v");
+    const allUsers = await users.find().select(" -__v");
     res.status(200).json({
       status: res.statusCode,
       message: "OK",
